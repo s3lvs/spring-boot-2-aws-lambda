02119c188d0d2ae7edc9ea7dcbb1268cc6ff76fb
selvs <selvs@byselvs.com>
2023-05-17 22:39:52 -0300

    primeiro commit

---
+++ src/main/java/com/example/helloworld/StreamLambdaHandler.java
@@ -0,0 +1,35 @@
+package com.example.helloworld;
+
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class StreamLambdaHandler implements RequestStreamHandler {
+    
+    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(HelloWorldApplication.class);
+        } catch (Exception ex) {
+            // Se não pudermos inicializar o handler, registre o erro
+            String errorMessage = "Não foi possível inicializar o Spring Boot application handler: " + ex.getMessage();
+            System.out.println(errorMessage);
+            throw new RuntimeException(errorMessage, ex);
+        }
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+    }
+
+}
